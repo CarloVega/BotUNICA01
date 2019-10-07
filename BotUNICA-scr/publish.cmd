9381bc02acba1f5e816ae608f1922519572d495e
CarloVega <20155683@sistemasunica.edu.pe>
2019-10-07 06:50:22 -0500

    Primera Version 1.0

---
+++ BotUNICA-scr/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=botunica01-a3d1-Web-Deploy.pubxml -p:Password=Gq1L7ni2bJqF1KkXzje12xsT1wMrcAPx7EKjxMLbcsYix3ubYZ8kozFcZ8MN
+
